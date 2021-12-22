224e381b6757cf260bbf0eb12be1b85970cb6c8a
Thomas Munzer <thomas.munzer@gmail.com>
2021-12-22 09:34:30 +0100

    minor changes

--- django_app/backend/lib/devices.py
+++ django_app/backend/lib/devices.py
@@ -6,15 +6,15 @@ from .common import Common
 
 class Devices(Common):
 
-    #############
-    # get Inventory from Cloud
-    #############
+#############
+# get Inventory from Cloud
+#############
     def pull_inventory(self, body):
         body = self.get_body(body)
-        if "org_id" in body:
-            return self._pull_inventory(body)
-        else:
+        if not "org_id" in body:
             return {"status": 500, "data": {"message": "site_id or org_id missing"}}
+        else:
+            return self._pull_inventory(body)
 
     def _pull_inventory(self, body):
         try:
@@ -37,7 +37,6 @@ class Devices(Common):
                 total = resp.headers["X-Page-Total"]
                 page += 1
             return {"status": 200, "data": {"total": total, "results": results}}
-
         except:
             return {"status": 500, "data": {"message": "Unable to retrieve the inventory"}}
 
@@ -45,31 +44,29 @@ class Devices(Common):
 #############
 # Claim devices to the inventory
 #############
-
     def claim_devices(self, body):
         body = self.get_body(body)
-        if "org_id" in body:
-            return self._claim_inventory(body)
+        if not "org_id" in body:
+            return {"status": 500, "data": {"message": "org_id is missing"}}
+        elif not "claim_codes" in body:
+            return {"status": 500, "data": {"message": "claim_codes is missing"}}
         else:
-            return {"status": 500, "data": {"message": "site_id or org_id missing"}}
+            return self._claim_inventory(body)
 
     def _claim_inventory(self, body):
         extract = self.extractAuth(body)
-        if "claim_codes" in body:
-            try:
-                url = "https://{0}/api/v1/installer/orgs/{1}/devices".format(
-                    body["host"], body["org_id"])
-                resp = requests.post(
-                    url, headers=extract["headers"], cookies=extract["cookies"], json=body["claim_codes"])
-                if "site_name" in body:
-                    data = resp.json()                    
-                    self._assign_device(
-                        body, data["inventory_added"], extract)
-                return {"status": 200, "data": {"results": resp.json()}}
-            except:
-                return {"status": 500, "data": {"message": "Unable to claim the devices"}}
-        else:
-            return {"status": 500, "data": {"message": "claim_codes missing"}}
+        try:
+            url = "https://{0}/api/v1/installer/orgs/{1}/devices".format(
+                body["host"], body["org_id"])
+            resp = requests.post(
+                url, headers=extract["headers"], cookies=extract["cookies"], json=body["claim_codes"])
+            if "site_name" in body:
+                data = resp.json()                    
+                self._assign_device(
+                    body, data["inventory_added"], extract)
+            return {"status": 200, "data": {"results": resp.json()}}
+        except:
+            return {"status": 500, "data": {"message": "Unable to claim the devices"}}
 
     def _assign_device(self, body, devices, extract):
         data = {
@@ -91,58 +88,56 @@ class Devices(Common):
 #############
 # Unclaim device from the inventory
 #############
-
-
     def unclaim_device(self, body):
         body = self.get_body(body)
-        if "org_id" in body:
-            return self._unclaim_inventory(body)
+        if not "org_id" in body:
+            return {"status": 500, "data": {"message": "org_id is missing"}}
+        elif not "device_mac" in body:
+            return {"status": 500, "data": {"message": "device_mac is missing"}}
         else:
-            return {"status": 500, "data": {"message": "site_id or org_id missing"}}
+            return self._unclaim_inventory(body)
 
     def _unclaim_inventory(self, body):
         extract = self.extractAuth(body)
-        if "device_mac" in body:
-            try:
-                url = "https://{0}/api/v1/installer/orgs/{1}/devices/{2}".format(
-                    body["host"], body["org_id"], body["device_mac"])
-                resp = requests.delete(
-                    url, headers=extract["headers"], cookies=extract["cookies"])
-                return {"status": 200, "data": {"result": resp.json()}}
-            except:
-                return {"status": 500, "data": {"message": "unable to unclaim the device"}}
-        else:
-            return {"status": 500, "data": {"message": "device_mac missing"}}
+        try:
+            url = "https://{0}/api/v1/installer/orgs/{1}/devices/{2}".format(
+                body["host"], body["org_id"], body["device_mac"])
+            resp = requests.delete(
+                url, headers=extract["headers"], cookies=extract["cookies"])
+            return {"status": 200, "data": {"result": resp.json()}}
+        except:
+            return {"status": 500, "data": {"message": "unable to unclaim the device"}}
 
 #############
 # Provision device from the inventory
 #############
     def provision_device(self, body):
         body = self.get_body(body)
-        if "org_id" in body:
-            return self._provision_device(body)
+        if not "org_id" in body:
+            return {"status": 500, "data": {"message": "org_id is missing"}}
+        elif not "device" in body:
+            return {"status": 500, "data": {"message": "device is missing"}}
+        elif not "device_mac" in body:
+            return {"status": 500, "data": {"message": "device_mac is missing"}}
         else:
-            return {"status": 500, "data": {"message": "site_id or org_id missing"}}
+            return self._provision_device(body)
 
     def _provision_device(self, body):
-        extract = self.extractAuth(body)
-        if "device" in body and "device_mac" in body:
-            data = {}
-            for key in body["device"]:
-                if body["device"][key]:
-                    data[key]=body["device"][key]
-                elif key != "site_id":
-                    data[key]=""
-            try:
-                url = "https://{0}/api/v1/installer/orgs/{1}/devices/{2}".format(
-                    body["host"], body["org_id"], body["device_mac"])
-                resp = requests.put(
-                    url, headers=extract["headers"], cookies=extract["cookies"], json=data)
-                return {"status": 200, "data": {"result": resp.json()}}
-            except:
-                return {"status": 500, "data": {"message": "unable to provision the device"}}
-        else:
-            return {"status": 500, "data": {"message": "device or device_mac missing"}}
+        extract = self.extractAuth(body)        
+        data = {}
+        for key in body["device"]:
+            if body["device"][key]:
+                data[key]=body["device"][key]
+            elif key != "site_id":
+                data[key]=""
+        try:
+            url = "https://{0}/api/v1/installer/orgs/{1}/devices/{2}".format(
+                body["host"], body["org_id"], body["device_mac"])
+            resp = requests.put(
+                url, headers=extract["headers"], cookies=extract["cookies"], json=data)
+            return {"status": 200, "data": {"result": resp.json()}}
+        except:
+            return {"status": 500, "data": {"message": "unable to provision the device"}}
 
 
 #############
@@ -150,42 +145,40 @@ class Devices(Common):
 #############
     def locate(self, body):
         body = self.get_body(body)
-        if "org_id" in body:
-            return self._locate(body)
-        else:
+        if not "org_id" in body:
             return {"status": 500, "data": {"message": "org_id missing"}}
+        elif not "device_mac" in body:
+            return {"status": 500, "data": {"message": "device_mac is missing"}}
+        else:
+            return self._locate(body)
 
     def _locate(self, body):
         extract = self.extractAuth(body)
-        if "device_mac" in body:
-            #try:
+        try:
             url = "https://{0}/api/v1/installer/orgs/{1}/devices/{2}/locate".format(
                 body["host"], body["org_id"], body["device_mac"])
             resp = requests.post(
                 url, headers=extract["headers"], cookies=extract["cookies"])
             return {"status": 200, "data": {"result": resp.json()}}
-            #except:
-            #    return {"status": 500, "data": {"message": "unable to provision the device"}}
-        else:
-            return {"status": 500, "data": {"message": "device_mac missing"}}
+        except:
+            return {"status": 500, "data": {"message": "unable to provision the device"}}
 
     def unlocate(self, body):
         body = self.get_body(body)
-        if "org_id" in body:
-            return self._unlocate(body)
-        else:
+        if not "org_id" in body:
             return {"status": 500, "data": {"message": "org_id missing"}}
+        elif not "device_mac" in body:
+            return {"status": 500, "data": {"message": "device_mac is missing"}}
+        else:
+            return self._unlocate(body)
 
     def _unlocate(self, body):
         extract = self.extractAuth(body)
-        if "device_mac" in body:
-            try:
-                url = "https://{0}/api/v1/installer/orgs/{1}/devices/{2}/unlocate".format(
-                    body["host"], body["org_id"], body["device_mac"])
-                resp = requests.post(
-                    url, headers=extract["headers"], cookies=extract["cookies"])
-                return {"status": 200, "data": {"result": resp.json()}}
-            except:
-                return {"status": 500, "data": {"message": "unable to provision the device"}}
-        else:
-            return {"status": 500, "data": {"message": "device_mac missing"}}
+        try:
+            url = "https://{0}/api/v1/installer/orgs/{1}/devices/{2}/unlocate".format(
+                body["host"], body["org_id"], body["device_mac"])
+            resp = requests.post(
+                url, headers=extract["headers"], cookies=extract["cookies"])
+            return {"status": 200, "data": {"result": resp.json()}}
+        except:
+            return {"status": 500, "data": {"message": "unable to provision the device"}}
